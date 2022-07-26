8e53d67cd2ff744bbfd2aba44a7e9b66480f15e7
Dipak Jha <dipakjha.16b@gmail.com>
2022-07-26 15:27:21 +0530

    apply coupon code added in user api

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const userProductServices = require("./api/users/products/products");
 const productOrders = require("./api/users/order/order");
 const productOrdersAdmin = require("./api/admin/orders/orders");
 const couponCode = require("./api/admin/coupon/codeGeneration");
+const userCoupon = require("./api/users/coupons/coupon");
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -30,6 +31,7 @@ app.use("/api/products/", userProductServices);
 app.use("/api/orders", productOrders);
 app.use("/api/orders", productOrdersAdmin);
 app.use("/api/coupon", couponCode);
+app.use("/api/coupons", userCoupon);
 app.listen(process.env.PORT || 3000, () => {
   console.log("server is up at 3000");
 });
